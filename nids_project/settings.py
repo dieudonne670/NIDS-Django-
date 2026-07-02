cdc21b238de5250944671b8db6216659d992df6a
dieudonne670 <kindongd41@gmail.com>
2026-07-02 22:11:43 +0100

    Add .gitignore and remove unnecessary files

--- nids_project/settings.py
+++ nids_project/settings.py
@@ -123,4 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+import os
+
+ALLOWED_HOSTS = ["*"]
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
